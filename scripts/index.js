0d99d94a84f68f0e2b20950aa97c86aac276e0d1
Alex Lyubimov <lyubimovone@yandex.ru>
2025-01-26 20:33:48 +0300

    initial commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+const cardsList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+function createCard(cardName, cardLink) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Функция удаления карточки
+    cardElement.querySelector('.card__image').src = cardLink;
+    cardElement.querySelector('.card__image').alt = cardName;
+    cardElement.querySelector('.card__title').textContent = cardName;
 
-// @todo: Вывести карточки на страницу
+    cardDeleteButton.addEventListener('click', deleteCard);
+
+    return cardElement;
+}
+
+function deleteCard(evt) {
+    const card = evt.target.closest('.card');
+    card.remove();
+}
+
+initialCards.forEach((card) => {
+    cardsList.append(createCard(card.name, card.link));
+});
